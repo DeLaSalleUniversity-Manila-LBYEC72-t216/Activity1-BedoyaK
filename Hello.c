acf96f179f8506cbc4478280bdfdd5946ef11aef
KarlaBedoya <Karla_Bedoya@dlsu.edu.ph>
2016-05-26 16:44:30 +0800

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{printf("@@@@@@@@@@@@@@@@@@@@@\n\n");
+ printf("Karla Andrea L. Bedoya\nBS ECE2\n\n");
+ printf("@@@@@@@@@@@@@@@@@@@@@\n\n");
+ 
+ system ("PAUSE");
+ 
+}
