d7c02897038a7bec0ff0e5f42238c6c8fe330bea
KarlaBedoya <Karla_Bedoya@dlsu.edu.ph>
2016-05-26 16:57:07 +0800

    Create Age.c

---
+++ Age.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int year, age;
+ 
+ printf("Birth Year:");
+ scanf("%d", &year);
+ age = 2016-year;
+ printf("Age: %d", age);
+ 
+ system("PAUSE");
+}
